52d69d86e7b9ad2c032b817de4bda6e9d3c17772
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2022-09-11 14:23:05 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -5,14 +5,12 @@ def draw_blanks(word):
     print()
 
 
-def fill_blanks(word, blank_word, inp, remaining_life):
-    if inp in blank_word:
-        remaining_life = remaining_life - 1
+def fill_blanks(word, blank_word, inp):
     for index in range(0,len(word)):
         if inp == word[index]:
             blank_word = blank_word[:index] + inp + blank_word[index+1:]
 
-    return blank_word,remaining_life
+    return blank_word
 
 
 def game_over(string):
